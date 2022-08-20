f60438992eb5b73a7a24cca776efbf7bb9ff5dc2
qrystalz@gmail.com <qrystalz@gmail.com>
2022-08-20 17:23:07 +0100

    Add player input

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-let choices = ["Rock", "Paper", "Scissors"];
-
-function getComputerChoice() {
-	return Math.floor(Math.random() * choices.length);
-}
+const choices = ["Rock", "Paper", "Scissors"];
 
 // Takes in two ints (0, 1, 2) and returns the result in an array [outcome (0 = tie, 1 = player win, -1 computer win), string to be printed]
 function playRound(playerChoice, computerChoice) {
@@ -18,5 +14,26 @@ function playRound(playerChoice, computerChoice) {
 
 	else { // player win
 		return [-1, "You win! " + choices[playerChoice] + " beats " + choices[computerChoice] + "."]
+	}
+}
+
+function getComputerChoice() {
+	return Math.floor(Math.random() * choices.length);
+}
+
+function getplayerChoice() {
+	while (true) { // keep asking player for input until a valid choice is received
+		let playerInput = prompt("Input your choice:");
+		let choice = choices.findIndex((element) => element == playerInput.toLowerCase());
+
+		// findIndex returns -1 if the playerInput doesn't match any of the valid choices
+		if (choice == -1) {
+			console.log("Invalid choice. Please choose between \"Rock\", \"Paper\", or \"Scissors\"(case insensitive).")
+		}
+		
+		// otherwise, findIndex returns the index of element that matches playerInput
+		else {
+			return choice
+		}
 	}
 }
