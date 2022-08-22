a5772f804de76bd912f748f04ac082a4b3f01e0f
qrystalz@gmail.com <qrystalz@gmail.com>
2022-08-22 15:03:30 +0100

    Add score header

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 const choices = ["Rock", "Paper", "Scissors"];
-const rounds = 5;
+const first_to = 5;
 
 
-function game() {
-	let score = [0, 0]; // player score, computer score
+// function game() {
+// 	let score = [0, 0]; // player score, computer score
 
-	for (let i = 0; i < rounds; i++) {
-		let result = playRound(getplayerChoice(), getComputerChoice());
+// 	for (let i = 0; i < rounds; i++) {
+// 		let result = playRound(getplayerChoice(), getComputerChoice());
 
-		console.log(result[1]) // print round outcome
+// 		console.log(result[1]) // print round outcome
 
-		if (result[0] == -1) {
-			score[1]++; // increment computer score
-		}
+// 		if (result[0] == -1) {
+// 			score[1]++; // increment computer score
+// 		}
 		
-		else if (result[0] == 1) {
-			score[0]++; // increment player score
-		}
+// 		else if (result[0] == 1) {
+// 			score[0]++; // increment player score
+// 		}
 
-		console.log("Player " + score[0] + " : " + score[1] + " Computer"); // print current score
-	}
+// 		console.log("Player " + score[0] + " : " + score[1] + " Computer"); // print current score
+// 	}
 
-	// end of game
-	if (score[0] > score[1]) {
-		console.log("You win!");
-	}
+// 	// end of game
+// 	if (score[0] > score[1]) {
+// 		console.log("You win!");
+// 	}
 	
-	else {
-		console.log("You lose!");
-	}
-}
+// 	else {
+// 		console.log("You lose!");
+// 	}
+// }
 
 
 // Takes in two ints (0, 1, 2) and returns the result in an array [outcome (0 = tie, 1 = player win, -1 computer win), string to be printed]
@@ -55,19 +55,19 @@ function getComputerChoice() {
 }
 
 
-function getplayerChoice() {
-	while (true) { // keep asking player for input until a valid choice is received
-		let playerInput = prompt("Input your choice:");
-		let choice = choices.findIndex((element) => element.toLowerCase() === playerInput.toLowerCase());
+// function getplayerChoice() {
+// 	while (true) { // keep asking player for input until a valid choice is received
+// 		let playerInput = prompt("Input your choice:");
+// 		let choice = choices.findIndex((element) => element.toLowerCase() === playerInput.toLowerCase());
 
-		// findIndex returns -1 if the playerInput doesn't match any of the valid choices
-		if (choice === -1) {
-			console.log("Invalid choice. Please choose between \"Rock\", \"Paper\", or \"Scissors\"(case insensitive).");
-		} else { 
-			return choice;
-		}
-	}
-}
+// 		// findIndex returns -1 if the playerInput doesn't match any of the valid choices
+// 		if (choice === -1) {
+// 			console.log("Invalid choice. Please choose between \"Rock\", \"Paper\", or \"Scissors\"(case insensitive).");
+// 		} else { 
+// 			return choice;
+// 		}
+// 	}
+// }
 
 
-game();
+//game();
